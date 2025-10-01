e0fa89707e13796e0719dcc9062f3559be368caa
ArnavGuptas <91640989+ArnavGuptas@users.noreply.github.com>
2025-10-01 14:02:56 -0700

    Update Gupta_madlibs.java

--- Gupta_madlibs.java
+++ Gupta_madlibs.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 public class Gupta_madlibs {
+    //This function gets the input from the user given the type of word neccesary as a paramater
     static String getInput(String wordType){
+        //This line creates a scanner object
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a " + wordType +": ");
         String input = sc.nextLine();
         return input;
     }
+    //This function prints the story given all of the words the user entered as string parameters
     static void printStory(String input1, String input2, String input3, String input4, String input5){
         System.out.println("It was a " + input1 + " day near the " + input2 + ". We started " + input3 + " through the woods when suddenly, a " + input4 + " squirrel zoomed across our path. It looked very " + input5 + " and quickly disappeared up a tree.");
     }
+    //This is the main function that runs things
     public static void main(String[]args){
-        System.out.println("It was a (adjective) day near the (noun). We started (verb) through the woods when suddenly, a (adverb) squirrel zoomed across our path. It looked very (adjective) and quickly disappeared up a tree.")
+        System.out.println("It was a (adjective) day near the (noun). We started (verb) through the woods when suddenly, a (adverb) squirrel zoomed across our path. It looked very (adjective) and quickly disappeared up a tree.");
+        //Calls to methods to get inputs and print story
         String s1 = getInput("adjective");
         String s2 = getInput("noun");
         String s3 = getInput("verb");
